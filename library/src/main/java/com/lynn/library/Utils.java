fb81dbbd22a2e2305e52025c5fcf8b3081cc53f6
wzl912920 <544198350@qq.com>
2017-08-04 11:36:26 +0800

    simple recycler-view version 0.0.1 upload

---
+++ library/src/main/java/com/lynn/library/Utils.java
@@ -0,0 +1,13 @@
+package com.lynn.library;
+
+import android.util.Log;
+
+/**
+ * Created by Lynn.
+ */
+
+public class Utils {
+    public static void log(String s) {
+        Log.e("AAA", "sssss=" + s);
+    }
+}
